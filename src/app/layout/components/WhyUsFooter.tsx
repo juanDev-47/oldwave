e4ec28e4a53d37484d53883942d7d0855ec586a0
juanDev-47 <juanp.arenas@udea.edu.co>
2022-07-24 17:30:00 -0500

    Implement whyusfooter component

--- src/app/layout/components/WhyUsFooter.tsx
+++ src/app/layout/components/WhyUsFooter.tsx
@@ -1,47 +1,77 @@
 import { Grid, Typography, Box } from "@mui/material";
+import { styled } from "@mui/material/styles";
 import React from "react";
-import iconCalidadGarantizada from "../../assets/icons/icon-calidad-garantizada@2x.png";
+import iconCalidadGarantizada from "../../assets/icons/icon-calidad-garantizada.png";
+import multiplesMediosDePago from "../../assets/icons/icon-multiples-medios-pago.png";
+import envios from "../../assets/icons/icon-envios-todo-elpais.png";
+import comprasSeguras from "../../assets/icons/icon-compras-seguras.png";
+
+const StyledTypography = styled(Typography)`
+  padding: 0 20px;
+  color: #772CE8;
+  font-size: { xs: "11px", md: "16px" };
+  text-align: justify;
+`;
 
 const WhyUsFooter = () => {
   return (
     <Grid
       container
-      sx={{ width: "100%", background: "#c8f3fd" }}
+      sx={{ width: "100%", background: "#c8f3fd", padding: { xs: '20px 0', md: '40px 0' } }}
       direction="row"
       justifyContent="center"
       alignItems="center"
+      spacing={{ xs: 3, md: 2 }}
     >
       <Grid
         item
-        sm={6}
+        xs={6}
         xl={3}
-        direction="row"
+        sx={{ display: "flex" }}
         justifyContent="center"
         alignItems="center"
+        textAlign="center"
       >
-        <Box
-          component="img"
-          sx={{
-            display: "inline",
-            width: "auto",
-          }}
-          src={iconCalidadGarantizada}
-          alt={"icon-calidad-garantizada"}
-        />
+        <Grid sx={{ display: "flex" }}>
+          <Box
+            component="img"
+            sx={{
+              width: "auto",
+            }}
+            src={comprasSeguras}
+            alt={"icon-compras-seguras"}
+          />
+          <StyledTypography>
+            Compras <br /> Seguras
+          </StyledTypography>
+        </Grid>
       </Grid>
       <Grid
         item
-        sm={6}
+        xs={6}
         xl={3}
-        direction="row"
+        sx={{ display: "flex" }}
         justifyContent="center"
         alignItems="center"
+        textAlign="center"
       >
-        <h1>Hola</h1>
+        <Grid sx={{ display: "flex" }}>
+          <Box
+            component="img"
+            sx={{
+              width: "auto",
+            }}
+            src={multiplesMediosDePago}
+            alt={"multiplesMediosDePago"}
+          />
+          <StyledTypography>
+            Multiples medios <br /> de pago
+          </StyledTypography>
+        </Grid>
       </Grid>
       <Grid
         item
-        sm={6}
+        xs={6}
         xl={3}
         sx={{ display: "flex" }}
         justifyContent="center"
@@ -57,23 +87,33 @@ const WhyUsFooter = () => {
             src={iconCalidadGarantizada}
             alt={"icon-calidad-garantizada"}
           />
-          <Typography variant="h5" color="#772ce8" noWrap>
-            Calidad Garantizada
-          </Typography>
+          <StyledTypography>
+            Calidad <br /> Garantizada
+          </StyledTypography>
         </Grid>
       </Grid>
       <Grid
         item
-        sm={6}
+        xs={6}
         xl={3}
-        direction="row"
+        sx={{ display: "flex" }}
         justifyContent="center"
         alignItems="center"
         textAlign="center"
       >
-        <Typography variant="h1" color="initial">
-          <h1>Hola</h1>
-        </Typography>
+        <Grid sx={{ display: "flex" }}>
+          <Box
+            component="img"
+            sx={{
+              width: "auto",
+            }}
+            src={envios}
+            alt={"envios-todo-elpais"}
+          />
+          <StyledTypography>
+            Envio a <br /> todo el país
+          </StyledTypography>
+        </Grid>
       </Grid>
     </Grid>
   );
