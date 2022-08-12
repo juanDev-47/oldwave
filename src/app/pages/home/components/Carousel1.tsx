4b443994ff632f3b86c3d5394a8e8958c61d2ea3
juanDev-47 <juanp.arenas@udea.edu.co>
2022-08-12 10:55:27 -0500

    production fixes

--- src/app/pages/home/components/Carousel1.tsx
+++ src/app/pages/home/components/Carousel1.tsx
@@ -28,47 +28,56 @@ function Carousel1(props) {
     {
       label: "Carros",
       imgPath: carros,
-      categoryId: '4f276a8a-2b7c-4b09-953a-e794e19c96da'
+      // categoryId: '4f276a8a-2b7c-4b09-953a-e794e19c96da'
+      categoryId: 'd4f107a0-2901-420c-8d4c-57995ed656c1'
     },
     {
       label: "Motos",
       imgPath: motos,
-      categoryId: '9faa9ac8-53f5-43fa-91e1-0a60b76c8122'
+      // categoryId: '9faa9ac8-53f5-43fa-91e1-0a60b76c8122'
+      categoryId: '6577ecee-cb8b-4b2b-8764-fb1d9895c29e'
     },
     {
       label: "Ropa",
       imgPath: ropa,
-      categoryId: 'a40f1a9c-671c-47d3-8ff8-864c50c271d6'
+      // categoryId: 'a40f1a9c-671c-47d3-8ff8-864c50c271d6'
+      categoryId: '468b4d3a-4549-422c-a464-5243a142da13'
     },
     {
       label: "Deporte",
       imgPath: deporte,
-      categoryId: '20f28ce1-26c0-4ced-bf59-065def757ece'
+      // categoryId: '20f28ce1-26c0-4ced-bf59-065def757ece'
+      categoryId: '23314ba0-df2d-45dc-abbb-d7be7c26db81'
     },
     {
       label: "Muebles",
       imgPath: muebles,
-      categoryId: 'f8e2357f-cbe8-4a40-b9bf-dbd905d1df43'
+      // categoryId: 'f8e2357f-cbe8-4a40-b9bf-dbd905d1df43'
+      categoryId: '57efeaf7-b3a9-4585-89ba-f13a6a176b39'
     },
     {
       label: "Computadores",
       imgPath: computadores,
-      categoryId: '0a6a0267-0095-4be0-a1be-4c516ba0b6ef'
+      // categoryId: '0a6a0267-0095-4be0-a1be-4c516ba0b6ef'
+      categoryId: '9d5360b2-dec3-41cc-9035-2db96017a6e7'
     },
     {
       label: "Celulares",
       imgPath: celular,
-      categoryId: '9f2a8b05-7550-45f2-ac1f-6e2db9e7eb9a'
+      // categoryId: '9f2a8b05-7550-45f2-ac1f-6e2db9e7eb9a'
+      categoryId: '90c09e93-6dea-44f5-8b9b-eabdfac7a0c1'
     },
     {
       label: "TV, Audio y foto",
       imgPath: tv,
-      categoryId: 'c697f242-d119-40b9-960f-b79320306a0f'
+      // categoryId: 'c697f242-d119-40b9-960f-b79320306a0f'
+      categoryId: '9809becd-2dde-418e-99e7-d8bc48b96114'
     },
     {
       label: "Electrodomésticos",
       imgPath: electrodomesticos,
-      categoryId: 'e0e83f5e-7e88-481b-ad67-4712c4cdc782'
+      // categoryId: 'e0e83f5e-7e88-481b-ad67-4712c4cdc782'
+      categoryId: 'b7a7bd57-e29d-4608-bff8-fc1d49c5d1bd'
     },
   ];
 
